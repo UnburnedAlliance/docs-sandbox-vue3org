d8d566a57ee89b147b208db78a399dadbcc8462d
UnburnedAlliance <linenotsecure@gmail.com>
2023-02-04 09:42:43 -0800

    Finished https://vuejs.org/guide/quick-start.html#splitting-up-the-modules and added custom CSS for internal button counter

--- backend/seamain.js
+++ backend/seamain.js
@@ -3,6 +3,6 @@ export default {
     data() {
       return { count: 0 }
     },
-    template: `<button class="slidebox" @click="count++">Seamoss<br />Count is {{ count }}</button>`
+    template: `<button class="slidebox" @click="count++">Seamoss<div class="btnCountDot">{{count}}</div></button>`
   }
   
