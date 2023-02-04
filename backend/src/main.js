1d517efd911046bf59db10b6ff77447170121ed0
UnburnedAlliance <linenotsecure@gmail.com>
2023-02-04 08:31:11 -0800

    Commented out original App.vue, using ESModule type build from /guide/quick-start docs

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// Ignoring Vue from node_modules, using the "ES Module Build" from Vue docs
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
+//      Commenting out the whole template app created from 'npm init vue@latest'
+//import App from './App.vue'
+
+//      Keeping the template CSS
 import './assets/main.css'
 
-createApp(App).mount('#app')
+  createApp({
+    data() {
+      return {
+        message: 'Seamoss'
+      }
+    }
+  }).mount('#app')
